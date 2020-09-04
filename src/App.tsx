f42a4e9d9ba5b2e68dbc17cf086075809d4b9b1d
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-09-04 15:19:09 +0300

    feat: Created httRequestes api object in ts.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header } from './Layots/Header/Header';
 import './App.css'
 import {Schedule} from './Layots/Schedule/Schedule';
+import { httpRequests } from './api/ts';
 
 function App() {
+	useEffect(() => {
+		httpRequests.getEvents();
+	})
+	
 	return <>
 	<Header />
 	<Schedule />
--- src/api/ts.ts
+++ src/api/ts.ts
@@ -3,3 +3,18 @@ import axios from "axios";
 export const instance = axios.create({
     baseURL: 'https://rs-react-schedule.firebaseapp.com/api/team/53/',
 });
+
+
+export const httpRequests = {
+    getEvents: () => {
+        instance.get('events').then(function (response: any) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    } 
+        
+
+}
+
