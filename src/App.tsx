a41a98bf3ec91d12b56b86f7024e4270a90add47
Nikita Bilyukov <55063207+Di72@users.noreply.github.com>
2020-09-09 21:44:59 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import ScheduleView from './Layouts/ScheduleView/ScheduleView';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Header } from './Layouts/Header/Header';
 
 const App: React.FC = () => {
 	return (
 		<Router>
 			<Provider store={store}>
-					<Header />
 				<ScheduleView />
 			</Provider>
 		</Router>
