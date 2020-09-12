9093a7472c7f3202560c1de656fc977f22b8d600
Stanislav Nemytov <staninspain69@gmail.com>
2020-09-12 18:21:55 +0200

    refactor

--- src/Layouts/Table/ScheduleTable.tsx
+++ src/Layouts/Table/ScheduleTable.tsx
@@ -7,21 +7,18 @@ const { Column } = Table;
 
 export const ScheduleTable = (props: any) => {
 
-	const [ editMode, setEditMode ] = useState({ editMode: false });
-  const [ currentTask, setCurrentTask ] = useState(null as null | EventsType[] );
-  
-
-  const [event, setEvent] = useState(currentTask && currentTask[0].name);
+	const [editMode, setEditMode] = useState({ editMode: false });
+	const [currentTask, setCurrentTask] = useState(null as null | EventsType[]);
 
 
-	useEffect(
-		() => {
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-      setCurrentTask(props.data.events);
-      if(currentTask) setEvent(currentTask[0].name)
-		},
-		[props.data.events]
-	);
+	const [event, setEvent] = useState(currentTask && currentTask[0].name);
+
+
+	useEffect(() => {
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		setCurrentTask(props.data.events);
+		setEvent(props.data.events[0].name)
+	}, [props.data.events]);
 
 	const { events } = props.data;
 	const eventsWithKey = events.map((item: { key: any; id: any }) => {
@@ -38,35 +35,36 @@ export const ScheduleTable = (props: any) => {
 		);
 	};
 
-	const onDataChange = (e: React.FormEvent<HTMLInputElement>)=>{
-    console.log(e.currentTarget.value)
-    const newState = e.currentTarget.value
-    setEvent(newState);
+	const onDataChange = (e: React.FormEvent<HTMLInputElement>) => {
+		console.log(e.currentTarget.value)
+		const newState = e.currentTarget.value
+		setEvent(newState);
 	}
 
-    const input = currentTask !== null ? <input onChange={onDataChange} value={event ? event : ''} /> : null
+	const input = currentTask !== null ? <input onChange={onDataChange} value={event ? event : ''} /> : null
 
 
 
-const content = currentTask ? (
-<> {input}
-		<Table dataSource={currentTask}>
-			<Column key="dateTime" title="Data" dataIndex="dateTime" />
-			<Column key="name" title="Name" dataIndex="name" />
-			<Column
-				key="type"
-				title="Type"
-				dataIndex="type"
-				render={(type: string, id: string) => renderTags(type, id)}
-			/>
-			<Column key="timeZone" title="TimeZone" dataIndex="timeZone" />
-			<Column key="description" title="Description" dataIndex="description" />
-			<Column key="place" title="Place" dataIndex="place" />
-		</Table>
+	const content = currentTask ? (
+		<> {input}
+			<Table dataSource={currentTask}>
+				<Column key="dateTime" title="Data" dataIndex="dateTime" />
+				<Column key="name" title="Name" dataIndex="name" />
+				<Column
+					key="type"
+					title="Type"
+					dataIndex="type"
+					render={(type: string, id: string) => renderTags(type, id)}
+				/>
+				<Column key="timeZone" title="TimeZone" dataIndex="timeZone" />
+				<Column key="description" title="Description" dataIndex="description" />
+				<Column key="place" title="Place" dataIndex="place" />
+			</Table>
 		</>
-) : (<h6>Loading...</h6>)
+	) : (<h6>Loading...</h6>)
+	console.log('event', event);
 
 	return (
-	<>{content}</>
+		<>{content}</>
 	);
 };
